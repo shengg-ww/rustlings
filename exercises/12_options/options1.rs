045c3c6c3980b3cdf5483fff9d2731c6784f8d43
2309123.ShengWei <2b02.2309123.shengwei@gmail.com>
2025-03-05 22:02:43 +0800

    some done

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,6 +4,16 @@
 // `hour_of_day` is higher than 23.
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    if hour_of_day >23{
+        None
+    }
+    else if hour_of_day==22 {
+        Some(0)
+    }
+    else {
+        Some(5)
+    }
+
 }
 
 fn main() {
@@ -19,6 +29,11 @@ mod tests {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
         let icecreams = maybe_icecream(12);
+
+        match icecreams {
+            Some(p) => println!("Scoops left: {}", p),
+            None => println!("No ice cream left!"),
+        }
 
         assert_eq!(icecreams, 5); // Don't change this line.
     }
